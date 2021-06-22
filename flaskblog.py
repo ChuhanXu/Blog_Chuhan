535520442103deb2a3b964f896cfa5875a7bf51c
ChuhanXu <chx37@pitt.edu>
2021-06-22 17:49:08 -0400

    Finished episode 2: Learned how to use templates inheritance using {% extends layout.html %}

--- flaskblog.py
+++ flaskblog.py
@@ -1,7 +1,28 @@
-from flask import Flask
+from flask import Flask,render_template, url_for
 app = Flask(__name__)
 
+
+blog_list = [{
+    "author":"Chuhan",
+    "title":"blog1",
+    "content":"first_blog",
+    "date_posted":"Jun 22"
+},{
+    "author": "Xiaoyu",
+    "title": "blog2",
+    "content": "second_blog",
+    "date_posted": "Jun 22"
+}]
 #decorator @app
 @app.route("/")
-def hello():
-    return "Hello World"
+@app.route("/home")
+def home():
+    return render_template('home.html',blog_list=blog_list,title="Chuhan")
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
